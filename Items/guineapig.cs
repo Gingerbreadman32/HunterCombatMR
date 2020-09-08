2da4943bbd0e76f72bc73d58530e12f7b9d4870b
Mercedes-Benz <warden.chad7@gmail.com>
2020-09-08 09:29:32 -0400

    initial code

--- Items/guineapig.cs
+++ Items/guineapig.cs
@@ -1,14 +1,25 @@
+using HunterCombatMR.Projectiles.SwordandShield;
+using Microsoft.Xna.Framework;
+using System;
+using System.Drawing.Text;
+using Terraria;
+using Terraria.GameInput;
 using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace HunterCombatMR.Items
 {
 	public class guineapig : ModItem
 	{
+		private const int LMB1TotalFrames = 26;
+		private int[] GroundLMBFrameTimings = new int[7] { 26, 16, 14, 12, 11, 10, 0 };
+		private bool nextFrame = true;
+
 		public override void SetStaticDefaults() 
 		{
-			// DisplayName.SetDefault("guineapig"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
-			Tooltip.SetDefault("This is a basic modded sword.");
+			DisplayName.SetDefault("Example Sword and Shield"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("Testing SnS Style Mechanics.");
 		}
 
 		public override void SetDefaults() 
@@ -17,14 +28,15 @@ namespace HunterCombatMR.Items
 			item.melee = true;
 			item.width = 40;
 			item.height = 40;
-			item.useTime = 20;
-			item.useAnimation = 20;
-			item.useStyle = 1;
+			item.useTime = LMB1TotalFrames;
+			item.useAnimation = LMB1TotalFrames;
+			item.useStyle = 69;
 			item.knockBack = 6;
 			item.value = 10000;
-			item.rare = 2;
-			item.UseSound = SoundID.Item1;
-			item.autoReuse = true;
+			item.rare = ItemRarityID.Green;
+			//item.UseSound = SoundID.Item1;
+			item.noMelee = true;
+			item.shootSpeed = 1;
 		}
 
 		public override void AddRecipes() 
@@ -34,6 +46,85 @@ namespace HunterCombatMR.Items
 			recipe.AddTile(TileID.WorkBenches);
 			recipe.SetResult(this);
 			recipe.AddRecipe();
+		}
+
+        public override void UseStyle(Player player)
+        {
+			/*
+			if (nextFrame)
+			{
+				Main.NewText("Current Frame:" + player.itemAnimation.ToString(), new Color(50, 125, 255));
+				nextFrame = false;
+			}
+			*/
+
+			player.itemAnimationMax = LMB1TotalFrames;
+			// Windup/Weapon hold for ground LMB, mostly just holding the sword.
+            if (player.itemAnimation <= GroundLMBFrameTimings[0] && player.itemAnimation >= GroundLMBFrameTimings[1])
+            {
+				item.noUseGraphic = false;
+				player.itemRotation = MathHelper.ToRadians(-90 * player.direction);
+				player.bodyFrame.Y = player.bodyFrame.Height;
+
+				var CurrentHandOffset = Main.OffsetsPlayerOnhand[player.bodyFrame.Y / 56];
+				if (player.direction != 1)
+				{
+					CurrentHandOffset.X = player.bodyFrame.Width - CurrentHandOffset.X;
+				}
+				if (player.gravDir != 1f)
+				{
+					CurrentHandOffset.Y = player.bodyFrame.Height - CurrentHandOffset.Y;
+				}
+				CurrentHandOffset -= new Vector2(player.bodyFrame.Width - player.width, player.bodyFrame.Height - 42) / 2f;
+				CurrentHandOffset += new Vector2(8 * player.direction, 10);
+
+				player.itemLocation = player.position + CurrentHandOffset;
+			}
+			else if (player.itemAnimation == GroundLMBFrameTimings[1]-1)
+            {
+				player.bodyFrame.Y = player.bodyFrame.Height * 2;
+				Projectile Swipe1 = Projectile.NewProjectileDirect(player.itemLocation, new Vector2(0, 0), mod.GetProjectile("SNSSwipe1").projectile.type, item.damage, item.knockBack, player.whoAmI);
+				item.noUseGraphic = true;
+			} 
+			else if (player.itemAnimation == GroundLMBFrameTimings[2])
+            {
+				player.bodyFrame.Y = player.bodyFrame.Height * 3;
+			} 
+			else if (player.itemAnimation == GroundLMBFrameTimings[3])
+            {
+				player.bodyFrame.Y = player.bodyFrame.Height * 4;
+			}
+			/*
+			else if(player.itemAnimation <= GroundLMBFrameTimings[6])
+            {
+				
+				player.itemRotation = MathHelper.ToRadians(56) * player.direction;
+
+				var CurrentHandOffset = Main.OffsetsPlayerOnhand[player.bodyFrame.Y / 56];
+				if (player.direction != 1)
+				{
+					CurrentHandOffset.X = player.bodyFrame.Width - CurrentHandOffset.X;
+				}
+				if (player.gravDir != 1f)
+				{
+					CurrentHandOffset.Y = player.bodyFrame.Height - CurrentHandOffset.Y;
+				}
+				CurrentHandOffset -= new Vector2(player.bodyFrame.Width - player.width, player.bodyFrame.Height - 42) / 2f;
+				CurrentHandOffset += new Vector2(10 * player.direction, 18);
+
+				player.itemLocation = player.position + CurrentHandOffset;
+				item.noUseGraphic = false;
+				
+			}
+				*/
+			/*
+			if (!PlayerInput.Triggers.JustPressed.Up)
+			{
+				player.itemAnimation++;
+			} else
+            {
+				nextFrame = true;
+            }*/
 		}
 	}
 }
---
+++ Projectiles/SwordandShield/SNSSwipe1.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace HunterCombatMR.Projectiles.SwordandShield
+{
+    public sealed class SNSSwipe1
+        : ModProjectile
+    {
+        //private int[] FrameTimings = new int[7] { 32, 22, 18, 14, 12, 10, 0 };
+        private int[] FrameTimings = new int[7] { 26, 16, 14, 12, 11, 10, 0 };
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("SNSSwipe1");
+            Main.projFrames[projectile.type] = 5;
+        }
+
+        public override void SetDefaults()
+        {   
+            projectile.width = 110;
+            projectile.height = 70;
+            projectile.friendly = true;
+            projectile.melee = true;
+            projectile.penetrate = -1;
+            projectile.timeLeft = 30;
+            projectile.tileCollide = false;
+            projectile.ignoreWater = true;
+            projectile.ownerHitCheck = true;
+            projectile.aiStyle = -1;
+            projectile.usesIDStaticNPCImmunity = true;
+            projectile.idStaticNPCHitCooldown = 14;
+        }
+
+        public override void AI()
+        {
+            Player Owner = Main.player[projectile.owner];
+
+            projectile.position.X = Owner.MountedCenter.X - (projectile.width / 2) - (6 * Owner.direction);
+            projectile.position.Y = Owner.MountedCenter.Y - (projectile.height / 2) - 10;
+
+            projectile.direction = Owner.direction;
+            projectile.spriteDirection = Owner.direction;
+            Owner.heldProj = projectile.whoAmI;
+            Owner.itemTime = Owner.itemAnimation;
+
+            if (Owner.itemAnimation < FrameTimings[1] && Owner.itemAnimation >= FrameTimings[2])
+            {
+                projectile.frame = 0;
+            }
+            else if (Owner.itemAnimation <= FrameTimings[2] && Owner.itemAnimation >= FrameTimings[3])
+            {
+                projectile.frame = 1;
+            }
+            else if (Owner.itemAnimation <= FrameTimings[3] && Owner.itemAnimation >= FrameTimings[4])
+            {
+                projectile.frame = 2;
+            }
+            else if (Owner.itemAnimation <= FrameTimings[4] && Owner.itemAnimation >= FrameTimings[5])
+            {
+                projectile.frame = 3;
+            }
+            else if (Owner.itemAnimation <= FrameTimings[5] && Owner.itemAnimation > FrameTimings[6])
+            {
+                projectile.frame = 4;
+            }
+            else if (Owner.itemAnimation == FrameTimings[6])
+            {
+                projectile.Kill();
+            }
+        }
+        
+        public override bool PreKill(int timeLeft)
+        {
+            return base.PreKill(timeLeft);
+        }
+    }
+}
